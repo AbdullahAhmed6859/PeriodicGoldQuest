c002eab07898c9e631561428ffba5d37d0a25ec2
AbdullahAhmed6859 <109270476+AbdullahAhmed6859@users.noreply.github.com>
2023-12-04 12:29:35 +0500

    reset_coors update and cache delteed

--- hero.py
+++ hero.py
@@ -1,5 +1,5 @@
-class Hero:
-    def __init__(self, name, og_coors, atomic):
-        self.name = name
-        self.og_coors = og_coors
-        self.atomic = atomic
+# class Hero:
+#     def __init__(self, name, og_coors, atomic):
+#         self.name = name
+#         self.og_coors = og_coors
+#         self.atomic = atomic
